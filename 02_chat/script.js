71e1df8dddbc19a312bfde58a3c26ca1a16822e5
ts <hannnk22@gmail.com>
2023-06-12 16:22:18 +0900

    first commit

---
+++ 02_chat/script.js
@@ -0,0 +1,16 @@
+// document.addEventListener('DOMContentLoaded', function () {
+// const catImages = document.querySelectorAll('.cats_images img');
+// const catMessage = document.querySelector('.cat-message');
+
+// catImages.forEach(function (catImage) {
+// catImage.addEventListener('click', function () {
+//     catMessage.style.display = 'block';
+// });
+// });
+// });
+//要素を４つぶん
+
+document.getElementById('cat1');
+document.getElementById('cat2');
+document.getElementById('cat3');
+document.getElementById('cat4');
