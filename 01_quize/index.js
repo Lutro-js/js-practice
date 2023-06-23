d22d513ad359c192c30a90979d72bff308d82d1d
ts <hannnk22@gmail.com>
2023-06-23 17:30:56 +0900

    :yellow_heart:クイズアプリ

--- 01_quize/index.js
+++ 01_quize/index.js
@@ -1,27 +1,88 @@
-const question = 'キティちゃんの体重は？';
-const answers = [
-    'りんご3個分',
-    'りんご5個分',
-    'バナナ1個分',
-    '火星1個分'
+const quiz = [
+    {
+        question: 'キティちゃんの体重は？',
+        answers: [
+            'りんご3個分',
+            'りんご5個分',
+            'バナナ1個分',
+            '火星1個分'
+    ],
+    correct: 'りんご3個分'
+    },{
+        question: 'キキとララの身長は何センチ？',
+        answers: [
+            '月の半分',
+            '月',
+            '地球',
+            '太陽'
+    ],
+    correct: '月'
+
+    },{
+        question: 'シナモンの誕生日は？',
+        answers: [
+            '4月4日',
+            '7月10日',
+            '3月6日',
+            '12月25日'
+    ],
+    correct: '3月6日'
+
+    },{
+        question: 'マイメロの好きな食べ物は？',
+        answers: [
+            'フォンダンショコラ',
+            'カタラーナ',
+            'アーモンドパウンドケーキ',
+            'フィナンシェ'
+    ],
+    correct: 'アーモンドパウンドケーキ'
+    }
 ];
-const correct = 'りんご3個分';
 
-document.getElementById('js-question').textContent = question;
 
-const button = document.getElementsByTagName('button');
+const quizLength = quiz.length;
+let quizIndex = 0;
+let score = 0;
 
-//定数の文字列をHTMLに反映させる
-button[0].textContent = answers[0];
-button[1].textContent = answers[1];
-button[2].textContent = answers[2];
-button[3].textContent = answers[3];
+const $button = document.getElementsByTagName('button');
+const buttonLength = $button.length;
 
-//ボタンをクリックしたら正誤判定
-button[0].addEventListener('click', ()=>{
-    if(correct === document.getElementsByTagName('button')[0].textContent){
+
+//クイズの問題文、選択肢を定義
+const setupQuiz = () => {
+    document.getElementById('js-question').textContent = quiz[quizIndex].question;
+    let buttonIndex= 0;
+    let buttonLength = $button.length;
+    while(buttonIndex < buttonLength){
+        $button[buttonIndex].textContent = quiz[quizIndex].answers[buttonIndex];
+        buttonIndex++;
+    }
+}
+setupQuiz();
+
+const clickHandler = (e) => {
+    if(quiz[quizIndex].correct === e.target.textContent){
         window.alert('正解！');
+        score++;
     }else {
         window.alert('不正解！');
     }
-});
+
+    quizIndex++;
+
+    if(quizIndex < quizLength){
+        setupQuiz();
+    }else{
+        window.alert('終了！あなたの正解数は' + score + '/' + quizLength + 'です！');
+    }
+};
+
+//ボタンをクリックしたら正誤判定
+let handlerIndex = 0;
+while (handlerIndex < buttonLength){
+    $button[handlerIndex].addEventListener('click', (e)=>{
+        clickHandler(e);
+    });
+    handlerIndex++;
+}
