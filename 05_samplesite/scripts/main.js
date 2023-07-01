518d68875633319e8b5bca34244e0a6d78337b30
ts <hannnk22@gmail.com>
2023-07-01 21:57:30 +0900

    :hatched_chick:サンプルサイト

--- 05_samplesite/scripts/main.js
+++ 05_samplesite/scripts/main.js
@@ -1,4 +1,23 @@
 document.addEventListener('DOMContentLoaded',function(){
     const hero = new HeroSlider('.swiper');
     hero.start();
+
+    const _textAnimation = function (el, isIntersecting) {
+        if(isIntersecting) {
+            const ta = new TweenTextAnimation(el);
+            ta.animate();
+        }
+    }
+
+    const so = new ScrollObserver('.tween-animate-title', _textAnimation);
+
+    const _invewAnimation = function (el, inview) {
+        if(inview) {
+            el.classList.add('inview');
+        }else {
+            el.classList.remove('invuew');
+        }
+    }
+
+    const so2 = new ScrollObserver('.cover-slide', _invewAnimation);
 });
