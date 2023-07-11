0352dfce8fac9c849f625c5c8d1820babce31d4a
ts <hannnk22@gmail.com>
2023-07-11 15:58:25 +0900

    :penguin:プロフィールサイト

--- 04_profile/scripts/main.js
+++ 04_profile/scripts/main.js
@@ -1,31 +1,47 @@
 class MobileMenu {
     constructor() {
-    this.DOM = {};
-    this.DOM.btn = document.querySelector(".mobile-menu__btn");
-    this.DOM.cover = document.querySelector(".mobile-menu__cover");
-    this.DOM.container = document.querySelector("#global-container");
-    this.eventType = this._getEventType();
-    this._addEvent();
-}
+        this.DOM = {};
+        this.DOM.btn = document.querySelector(".mobile-menu__btn");
+        this.DOM.cover = document.querySelector(".mobile-menu__cover");
+        this.DOM.container = document.querySelector("#global-container");
+        this.eventType = this._getEventType();
+        this._addEvent();
+        this.isOpen = false; // メニューが開いているかどうかを管理するフラグ
+    }
 
+    _getEventType() {
+        const isTouchCapable =
+        "ontouchstart" in window || (window.DocumentTouch && document instanceof DocumentTouch);
+        return isTouchCapable ? "touchstart" : "click";
+    }
 
-_getEventType() {
-    const isTouchCapable = "ontouchstart" in window ||
-    (window.DocumentTouch && document instanceof DocumentTouch);
+    _toggle() {
+        this.isOpen = !this.isOpen; // メニューの開閉状態を反転させる
+        this.DOM.container.classList.toggle("menu-open", this.isOpen); // メニューが開いている場合のみクラスを追加
+    }
 
-    return isTouchCapable ? "touchstart" : "click";
+    _close() {
+        if (this.isOpen) {
+        this._toggle();
+        }
+    }
 
-}
+    _addEvent() {
+        this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
+        this.DOM.cover.addEventListener(this.eventType, this._toggle.bind(this));
+        window.addEventListener('scroll', this._handleScroll.bind(this)); // スクロールイベントを追加
+      }
+      
+      _toggle() {
+        this.DOM.container.classList.toggle('menu-open');
+      }
+      
+      _handleScroll() {
+        if (window.scrollY > 0) {
+          this.DOM.container.classList.remove('menu-open');
+        }
+      }
+      
+    }      
 
-_toggle() {
-    this.DOM.container.classList.toggle("menu-open");
-}
-
-_addEvent() {
-    this.DOM.btn.addEventListener(this.eventType, this._toggle.bind(this));
-    this.DOM.cover.addEventListener(this.eventType, this._toggle.bind(this));
-}
-}
-
-
-new MobileMenu();
+    new MobileMenu();
